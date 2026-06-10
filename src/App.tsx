2eae1641af3eff32a474f1b10e9ad3b8f7bfa295
zastraea <zastraea@proton.me>
2026-06-10 19:18:15 -0400

    removed a following page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { Router, Route, Navigate } from '@solidjs/router'
-import Login from './pages/Login'
-import UserFeed from './pages/UserFeed'
-import Post from './pages/Post'
-import FollowingFeed from './pages/FollowingFeed'
-import { getCurrentUser, setCurrentUser } from './lib/api'
+import { Router, Route, Navigate } from "@solidjs/router";
+import Login from "./pages/Login";
+import UserFeed from "./pages/UserFeed";
+import Post from "./pages/Post";
+import FollowingFeed from "./pages/FollowingFeed";
+import { getCurrentUser, setCurrentUser } from "./lib/api";
 
 function Home() {
-  let user = getCurrentUser()
+  let user = getCurrentUser();
   if (!user) {
-    try { user = JSON.parse(localStorage.getItem('user') || 'null') } catch {}
-    if (user) setCurrentUser(user)
+    try {
+      user = JSON.parse(localStorage.getItem("user") || "null");
+    } catch {}
+    if (user) setCurrentUser(user);
   }
-  if (!user) return <Navigate href="/login" />
-  return <FollowingFeed />
+  if (!user) return <Navigate href="/login" />;
+  return <FollowingFeed />;
 }
 
 export default function App() {
@@ -20,9 +22,8 @@ export default function App() {
     <Router>
       <Route path="/login" component={Login} />
       <Route path="/" component={Home} />
-      <Route path="/following" component={FollowingFeed} />
       <Route path="/:user" component={UserFeed} />
       <Route path="/post/:id" component={Post} />
     </Router>
-  )
+  );
 }
